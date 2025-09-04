c1a5415c76fe692039f47905752f6595d8e73e22
Sebastian Alberternst <sebastian.alberternst@dfki.de>
2025-09-04 08:40:57 +0000

    Cleanup dev-setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,13 @@ export default defineConfig({
         port: 5173,
         watch: {
             usePolling: true
+        },
+        proxy: {
+            '/api': {
+                target: 'http://backend:8080',
+                changeOrigin: true,
+                secure: false
+            }
         }
     },
     base: './',
